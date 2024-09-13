09457c52f3af51a21bdfbc4ba3cdfd55064963b7
Taylor Price <drpebcak@users.noreply.github.com>
2024-09-13 11:26:24 -0700

    chore: support streaming and additional models (#22)
    Co-authored-by: Grant Linville <grant@acorn.io>

--- index.js
+++ index.js
@@ -1,125 +1,127 @@
-import { fileTypeFromBuffer } from 'file-type';
+import {fileTypeFromBuffer} from 'file-type';
 import fs from 'fs';
 import OpenAI from 'openai';
 
 async function main() {
-  let images = []
-  if (process.env.IMAGES) {
-    images = process.env.IMAGES.split(/(?<!\\),/) // this matches all unescaped commas
-    for (let i = 0; i < images.length; i++) {
-      images[i] = images[i].replace(/\\,/g, ',')
+    let images = []
+    if (process.env.IMAGES) {
+        images = process.env.IMAGES.split(/(?<!\\),/) // this matches all unescaped commas
+        for (let i = 0; i < images.length; i++) {
+            images[i] = images[i].replace(/\\,/g, ',')
+        }
     }
-  }
 
-  if (images.length === 0) {
-    console.log("No images provided. Please provide a list of images to send to the vision model.")
-    process.exit(1)
-  }
-
-  const prompt = process.env.PROMPT ?? ""
-  if (prompt === "") {
-    console.log("No prompt provided. Please provide a prompt to send to the vision model.")
-    process.exit(1)
-  }
-
-  const options = {}
-
-  if (process.env.OPENAI_API_KEY) {
-    options.openaiApiKey = process
-  }
-  if (process.env.OPENAI_BASE_URL) {
-    options.baseUrl = process.env.OPENAI_BASE_URL
-  }
-  if (process.env.OPENAI_ORG_ID) {
-      options.orgId = process.env.OPENAI_ORG_ID
-  }
-
-  if (process.env.MAX_TOKENS) {
-    options.maxTokens = parseInt(process.env.MAX_TOKENS)
-  } else {
-    options.maxTokens = 2048
-  }
-
-  if (process.env.MODEL) {
-    if (process.env.MODEL === 'gpt-4-turbo') {
-      options.model = 'gpt-4-turbo'
-    } else if (process.env.MODEL === 'gpt-4o') {
-      options.model = 'gpt-4o'
-    } else {
-        console.log("Invalid model provided. Please provide a valid model.")
+    if (images.length === 0) {
+        console.log("No images provided. Please provide a list of images to send to the vision model.")
         process.exit(1)
     }
-  } else {
-    options.model = 'gpt-4o'
-  }
 
-  if (process.env.DETAIL) {
-    if (process.env.DETAIL === 'low') {
-      options.detail = 'low'
-    } else if (process.env.DETAIL === 'high') {
-      options.detail = 'high'
-    } else if (process.env.DETAIL === 'auto') {
-      options.detail = 'auto'
-    } else {
-        console.log("Invalid detail provided. Please provide a valid detail.")
+    const prompt = process.env.PROMPT ?? ""
+    if (prompt === "") {
+        console.log("No prompt provided. Please provide a prompt to send to the vision model.")
         process.exit(1)
     }
-  } else {
-    options.detail = 'auto'
-  }
 
-  await run(prompt, images, options)
+    const options = {}
+
+    if (process.env.OPENAI_API_KEY) {
+        options.openaiApiKey = process.env.OPENAI_API_KEY
+    }
+    if (process.env.OPENAI_BASE_URL) {
+        options.baseUrl = process.env.OPENAI_BASE_URL
+    }
+    if (process.env.OPENAI_ORG_ID) {
+        options.orgId = process.env.OPENAI_ORG_ID
+    }
+
+    if (process.env.MAX_TOKENS) {
+        options.maxTokens = parseInt(process.env.MAX_TOKENS)
+    } else {
+        options.maxTokens = 2048
+    }
+
+    if (process.env.MODEL) {
+        options.model = process.env.MODEL
+    } else {
+        console.log("No model provided. Please provide a model.")
+        process.exit(1)
+    }
+
+    if (process.env.DETAIL) {
+        if (process.env.DETAIL === 'low') {
+            options.detail = 'low'
+        } else if (process.env.DETAIL === 'high') {
+            options.detail = 'high'
+        } else if (process.env.DETAIL === 'auto') {
+            options.detail = 'auto'
+        } else {
+            console.log("Invalid detail provided. Please provide a valid detail.")
+            process.exit(1)
+        }
+    } else {
+        options.detail = 'auto'
+    }
+
+    await run(prompt, images, options)
 }
 
 async function run(prompt, images, opts) {
-  let content = []
-  for (let image of images) {
-    content.push({
-      type: "image_url",
-      image_url: {
-        detail: opts.detail,
-        url: await resolveImageURL(image)
-      }
-    })
-  }
+    let content = []
+    for (let image of images) {
+        content.push({
+            type: "image_url",
+            image_url: {
+                detail: opts.detail,
+                url: await resolveImageURL(image)
+            }
+        })
+    }
 
-  const openai = new OpenAI(opts.openaiApiKey, opts.baseUrl, opts.orgId);
-  const response = await openai.chat.completions.create({
-    model: opts.model,
-    max_tokens: opts.maxTokens,
-    messages: [
-      {
-        role: 'user',
-        content: [
-          { type: "text", text: prompt },
-          ...content
+    const openai = new OpenAI(opts.openaiApiKey, opts.baseUrl, opts.orgId);
+    const response = await openai.chat.completions.create({
+        model: opts.model,
+        max_tokens: opts.maxTokens,
+        stream: true,
+        messages: [
+            {
+                role: 'user',
+                content: [
+                    {type: "text", text: prompt},
+                    ...content
+                ]
+            },
         ]
-      },
-    ]
-  });
+    });
 
-  console.log(JSON.stringify(response, null, 4));
+    for await (const part of response) {
+        const {choices} = part;
+        const text = choices[0]?.delta?.content;
+        if (text) {
+            process.stdout.write(text);
+        }
+    }
+
 }
 
 async function resolveImageURL(image) {
-  const uri = new URL(image)
-  switch (uri.protocol) {
-    case 'http:':
-    case 'https:':
-      return image;
-    case 'file:':
-      const filePath = image.slice(7)
-      const data = fs.readFileSync(filePath)
-      const mime = (await fileTypeFromBuffer(data)).mime
-      if (mime !== 'image/jpeg' && mime !== 'image/png') {
-        throw new Error('Unsupported mimetype')
-      }
+    const uri = new URL(image)
+    switch (uri.protocol) {
+        case 'http:':
+        case 'https:':
+            return image;
+        case 'file:':
+            const filePath = image.slice(7)
+            const data = fs.readFileSync(filePath)
+            const mime = (await fileTypeFromBuffer(data)).mime
+            if (mime !== 'image/jpeg' && mime !== 'image/png') {
+                throw new Error('Unsupported mimetype')
+            }
 
-      const base64 = data.toString('base64')
-      return `data:${mime};base64,${base64}`
-    default:
-      throw new Error('Unsupported protocol')
-  }
+            const base64 = data.toString('base64')
+            return `data:${mime};base64,${base64}`
+        default:
+            throw new Error('Unsupported protocol')
+    }
 }
 
 await main();
